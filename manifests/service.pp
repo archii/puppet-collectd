fab2e6ec20a1453bd26c589ef8580eab4e572d5e
archii <archii@users.noreply.github.com>
2019-05-27 13:34:53 +1000

    allow flexible service resource title

--- manifests/service.pp
+++ manifests/service.pp
@@ -4,7 +4,7 @@ class collectd::service {
   assert_private()
 
   if $collectd::manage_service {
-    service { 'collectd':
+    service { $collectd::service_name:
       ensure => $collectd::service_ensure,
       name   => $collectd::service_name,
       enable => $collectd::service_enable,
